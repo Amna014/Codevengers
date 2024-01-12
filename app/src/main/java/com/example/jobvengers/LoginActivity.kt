3b3f3441dffeb10b31a6770c254424854c9b2fa9
BANGASH 03242038576 <SHAIKH.54984@iqra.edu.pk>
2024-01-12 18:02:20 +0500

    login activity and signup option activity (1/12/24: 6:02) by Shaikh Abdul Sami

--- app/src/main/java/com/example/jobvengers/LoginActivity.kt
+++ app/src/main/java/com/example/jobvengers/LoginActivity.kt
@@ -1,8 +1,29 @@
 package com.example.jobvengers
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.example.jobvengers.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
+    private lateinit var binding: ActivityLoginBinding
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signupNow.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            binding.signupNow.id -> {
+                val intent = Intent(this, SignUpOptionActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
 }
