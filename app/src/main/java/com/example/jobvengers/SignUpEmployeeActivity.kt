1a9d390b0f4a657c77b2fb170dd84afafade8e77
BANGASH 03242038576 <SHAIKH.54984@iqra.edu.pk>
2024-01-12 17:42:25 +0500

    testing git

--- app/src/main/java/com/example/jobvengers/SignUpEmployeeActivity.kt
+++ app/src/main/java/com/example/jobvengers/SignUpEmployeeActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class SignUpEmployeeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_sign_up_employee)
     }
 }
