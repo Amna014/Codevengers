956c0068bb48e67dd64ed7a1ad4e11176650855c
AbdulMuqeet2000 <Abdul.54797@iqra.edu.pk>
2024-01-21 21:20:09 +0500

    Job Details Design

--- app/src/main/java/com/example/jobvengers/JobDetails.kt
+++ app/src/main/java/com/example/jobvengers/JobDetails.kt
@@ -1,11 +1,32 @@
 package com.example.jobvengers
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.jobvengers.data.Jobs
+import com.example.jobvengers.databinding.ActivityJobDetailsBinding
 
+@Suppress("DEPRECATION")
 class JobDetails : AppCompatActivity() {
+
+    private lateinit var binding: ActivityJobDetailsBinding
+    private var jobDetail: Jobs? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_job_details)
+        binding = ActivityJobDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        jobDetail = intent.getParcelableExtra("Jobs")
+
+        binding.apply {
+            JobRole.text = jobDetail?.title
+            JobDesignation.text = jobDetail?.designation
+            Salary.text = jobDetail?.salary
+            editJobDetails.text = jobDetail?.description
+
+            btnApplyNowFragment.setOnClickListener {
+
+            }
+        }
+
     }
 }
