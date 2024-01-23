7fba4ba6b585750f104a27af85402e04ebd539cc
Furqan-kz <152624556+Furqan-kz@users.noreply.github.com>
2024-01-24 00:01:47 +0500

    Update config.php
    Database connection file

--- Jobvenger_Backend/DB_configuration/config.php
+++ Jobvenger_Backend/DB_configuration/config.php
@@ -1,1 +1,14 @@
+<?php
+$host = "localhost";
+$dbname = "job_application";
+$username = "root";
+$password = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+} catch (PDOException $e) {
+    die("Error: " . $e->getMessage());
+}
 
